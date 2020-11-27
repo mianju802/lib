c7fc0bab29c8d87b5d126268c048402794efe2e4
gandia <xufongnian@gmail.com>
2020-11-27 23:48:21 +0800

    apollo 配置调整

--- apollo/apolloConfig.go
+++ apollo/apolloConfig.go
@@ -8,15 +8,16 @@ import (
 )
 
 type Apollo struct {
-	localCache cache.Cache
+	LocalCache *cache.Cache
 }
 
 func init() {
+	// 通过默认根目录下的app.properties初始化agollo
 	err := agollo.InitWithDefaultConfigFile(
-		agollo.WithLogger(agollo.NewLogger(agollo.LoggerWriter(os.Stdout))),
-		agollo.PreloadNamespaces(),
-		agollo.AutoFetchOnCacheMiss(),
-		agollo.FailTolerantOnBackupExists(),
+		agollo.WithLogger(agollo.NewLogger(agollo.LoggerWriter(os.Stdout))), // 打印日志信息
+		agollo.PreloadNamespaces(),                                          // 预先加载的namespace列表，如果是通过配置启动，会在app.properties配置的基础上追加
+		agollo.AutoFetchOnCacheMiss(),                                       // 在配置未找到时，去apollo的带缓存的获取配置接口，获取配置
+		agollo.FailTolerantOnBackupExists(),                                 // 在连接apollo失败时，如果在配置的目录下存在.agollo备份配置，会读取备份在服务器无法连接的情况下
 	)
 	if err != nil {
 		log.Fatalf("in initApollo call agollo.InitWithDefaultConfigFile error: %v", err)
@@ -24,23 +25,24 @@ func init() {
 }
 
 func (apollo *Apollo) ReadApolloConfig(nameSpace string) {
-	var (
-		errChan   = agollo.Start()
-		watchChan = agollo.WatchNamespace(nameSpace, make(chan bool))
-	)
 	for _, val := range new(agollo.Configurations).Different(agollo.GetNameSpace(nameSpace)) {
 		if val.Type != agollo.ChangeTypeDelete {
-			apollo.localCache.SetDefault(val.Key, val.Value)
+			apollo.LocalCache.SetDefault(val.Key, val.Value)
 		}
 	}
+	var (
+		errChan            = agollo.Start()
+		watchNameSpaceChan = agollo.WatchNamespace("consul", make(chan bool))
+	)
+
 	for {
 		select {
-		case err := <-errChan:
-			log.Fatalf("in initApollo agollo.Start error: %v", err)
-		case rsp := <-watchChan:
-			for _, val := range rsp.OldValue.Different(rsp.NewValue) {
+		case err := <- errChan:
+			log.Fatalf("err: %v",err)
+		case watchData := <- watchNameSpaceChan:
+			for _, val := range watchData.OldValue.Different(watchData.NewValue) {
 				if val.Type != agollo.ChangeTypeDelete {
-					apollo.localCache.SetDefault(val.Key, val.Value)
+					apollo.LocalCache.SetDefault(val.Key, val.Value)
 				}
 			}
 		}
--- apollo/go.mod
+++ apollo/go.mod
@@ -3,6 +3,6 @@ module github.com/mianju802/lib/apollo
 go 1.13
 
 require (
-	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/shima-park/agollo v1.2.7
 )
